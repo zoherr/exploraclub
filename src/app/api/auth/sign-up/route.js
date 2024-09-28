452e6e753b64f299ec923f134c18ccadad49151e
Zoher Rangwala <workforzoher@gmail.com>
2024-09-28 10:25:47 +0530

    FINAL TODAY

--- src/app/api/auth/sign-up/route.js
+++ src/app/api/auth/sign-up/route.js
@@ -38,7 +38,7 @@ export const POST = async (NextRequest) => {
             semester
 
         }
-        await slack(`${newUser.name} Register`)
+        await slack(`${name} Register`)
         const token = jwt.sign(tokenData, process.env.JWT_SECRETKEY, { expiresIn: '1w' });
 
         await newUser.save();
