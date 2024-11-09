44114c9bf2769d38740aac4c39d2a379860f3d6a
Zoher Rangwala <workforzoher@gmail.com>
2024-11-09 13:37:27 +0530

    a

--- src/app/api/cron/route.js
+++ src/app/api/cron/route.js
@@ -2,11 +2,13 @@ import connectDB from '../../../utils/connectDB';
 import User from '../../../models/user.models';
 import { isBefore } from 'date-fns'; // Importing date-fns for comparison
 
-connectDB();
+
 
 const deleteUnverifiedAccounts = async () => {
   try {
     const currentUtcTime = new Date();
+    console.log(currentUtcTime);
+
     await User.deleteMany({
       isVerified: false,
       verificationExpires: { $lt:currentUtcTime  },
@@ -22,8 +24,11 @@ const deleteUnverifiedAccounts = async () => {
 
 export async function GET() {
     try {
+        const currentUtcTime = new Date();
+        console.log(currentUtcTime);
+        await connectDB();
         await deleteUnverifiedAccounts();
-        return new Response("Cron job executed successfully", { status: 200 })
+        return new Response("Cron job executed successfully",currentUtcTime, { status: 200 })
       } catch (error) {
         return new Response("Failed to execute cron job", { status: 500 })
       }
