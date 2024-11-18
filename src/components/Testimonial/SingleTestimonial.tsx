9f686ce955c58f3b1e516336ff78f42b419c1dba
Zoher Rangwala <workforzoher@gmail.com>
2024-11-18 19:46:37 +0530

    Ui change

--- src/components/Testimonial/SingleTestimonial.tsx
+++ src/components/Testimonial/SingleTestimonial.tsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 const SingleTestimonial = ({ review }:any) => {
   const { name,  content } = review;
   return (
-    <div className="NeueMontreal-Regular rounded-lg bg-white p-9 h-56 pt-7 border-2 shadow-solid-9   ">
+    <div className="NeueMontreal-Regular rounded-lg bg-white p-9 sm:h-56 pt-7 border-2 shadow-solid-9   ">
       <div className=" flex justify-between border-b border-stroke pb-6 ">
         <div>
-          <h3 className=" ">
+          <h3 className=" text-xl">
             {name}
           </h3>
         </div>
--- src/components/ui/cover.tsx
+++ src/components/ui/cover.tsx
@@ -70,7 +70,7 @@ export const Cover = ({
                 background="transparent"
                 minSize={0.4}
                 maxSize={1}
-                particleDensity={500}
+                particleDensity={35}
                 className="w-full h-full"
                 particleColor="#FFFFFF"
               />
