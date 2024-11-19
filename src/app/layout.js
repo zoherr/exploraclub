e4fe0f95e42950f22e879a2391a33621e8a7f69e
Zoher Rangwala <workforzoher@gmail.com>
2024-11-19 10:25:04 +0530

    .

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Analytics } from "@vercel/analytics/react"
 import localFont from "next/font/local";
 import "./globals.css";
+
 import toast, { Toaster } from 'react-hot-toast';
 import { Provider } from 'react-redux';
 import { store } from "../redux/store"
@@ -28,9 +29,7 @@ export default function RootLayout({ children }) {
                     position="top-center" />
                 <Provider store={store}>
 
-
                     <Navbar />
-                    <motion.div style={{ scaleX: scrollYProgress }} />
                     {children}
                     <SpeedInsights />
                     <Analytics />
