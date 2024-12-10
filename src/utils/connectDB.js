4696b97e1504d9f915e089569543c60a33983983
Zoher Rangwala <workforzoher@gmail.com>
2024-12-11 02:00:33 +0530

    db

--- src/utils/connectDB.js
+++ src/utils/connectDB.js
@@ -1,11 +1,27 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
+
+const globalWithMongoose = global as typeof global & { mongoose: { conn: any; promise: Promise<any> | null } };
+
+if (!globalWithMongoose.mongoose) {
+  globalWithMongoose.mongoose = { conn: null, promise: null };
+}
 
 export default async function connectDB() {
-    try {
-        mongoose.connect(process.env.DB_URL).then(() => {
-            console.log("DB connected successfully");
-        })
-    } catch (error) {
-        console.log(error);
-    }
+  if (globalWithMongoose.mongoose.conn) {
+    console.log("Using existing database connection");
+    return globalWithMongoose.mongoose.conn;
+  }
+
+  if (!globalWithMongoose.mongoose.promise) {
+    globalWithMongoose.mongoose.promise = mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }).then((mongoose) => {
+      console.log("DB connected successfully");
+      return mongoose;
+    });
+  }
+
+  globalWithMongoose.mongoose.conn = await globalWithMongoose.mongoose.promise;
+  return globalWithMongoose.mongoose.conn;
 }
